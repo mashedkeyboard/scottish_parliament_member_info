cc9e25d34569e0e0841e808db4306777baab62e5
Curtis Parfitt-Ford <curtis@mashedkeyboard.me>
2019-04-19 16:10:25 +0100

    Remove titles from names to fit with the standard

--- scraper.rb
+++ scraper.rb
@@ -7,6 +7,7 @@ response = HTTP.get('https://data.parliament.scot/api/members')
 
 JSON.parse(response.to_s, symbolize_names: true).each do |person|
   parsed_name = person[:ParliamentaryName].split(', ').reverse.join(' ')
+  parsed_name.sub!(/(?:Mr|Mrs|Ms|Dr|Lord) /, '')
   ScraperWiki.save_sqlite([:identifier__scotparl],
                           identifier__scotparl: person[:PersonID],
                           image: person[:PhotoURL],
